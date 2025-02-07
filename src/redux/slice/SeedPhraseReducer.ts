61eb3569c4fbc96f9723989e84281160b87a0312
AmitRedSoftware <amit.ahuja@redsoftware.in>
2025-02-07 16:13:32 +0530

    refactor: removed unused states from seedphrasereducer

--- src/redux/slice/SeedPhraseReducer.ts
+++ src/redux/slice/SeedPhraseReducer.ts
@@ -1,29 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 export interface seedPhraseReducerType {
-    password: string
-    passwordError: string
-    confirmPassword: string
-    confirmPasswordError: string
-    confirmPasswordFeedback: string
-    passwordFeedback: string;
     isSeedPhraseVerified:boolean;
     disabled : boolean,
-    isRestoreWallet: boolean
-    
+    isRestoreWallet: boolean   
 }
 
 const initialState: seedPhraseReducerType = {
-    password: '',
-    passwordError: '',
-    confirmPassword: '',
-    confirmPasswordError: '',
-    confirmPasswordFeedback: '',
-    passwordFeedback: '',
-    words:'',
     isSeedPhraseVerified:false,
     disabled : true,
-    wordsArray: [],
     isRestoreWallet: false
 };
 
@@ -31,25 +16,6 @@ export const seedPhraseReducer = createSlice({
     initialState,
     name: 'SeedPhraseReducer',
     reducers: {
-        setPassword: (state, action) => {
-            state.password = action.payload
-        },
-        setPasswordError: (state, action) => {
-            state.passwordError = action.payload
-        },
-        setConfirmPassword: (state, action) => {
-            state.confirmPassword = action.payload
-        },
-        setConfirmPasswordError: (state, action) => {
-            state.confirmPasswordError = action.payload
-        },
-        setConfirmPasswordFeedBack: (state, action) => {
-            state.confirmPasswordFeedback = action.payload
-        },
-        setPasswordFeedback: (state, action) => {
-            state.passwordFeedback = action.payload;
-        },
-
         setDisabled: (state, action) => {
             state.disabled = action.payload;
         },
@@ -64,6 +30,6 @@ export const seedPhraseReducer = createSlice({
     },
 });
 
-export const { setPassword, setPasswordError, setIsRestoreWallet, setConfirmPassword, setConfirmPasswordError,setPasswordFeedback ,clearSeedPhraseReducer, setDisabled, setIsSeedPhraseVerified , setConfirmPasswordFeedBack} = seedPhraseReducer.actions;
+export const { setIsRestoreWallet,clearSeedPhraseReducer, setDisabled, setIsSeedPhraseVerified } = seedPhraseReducer.actions;
 
 export default seedPhraseReducer.reducer;
