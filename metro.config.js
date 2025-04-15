d4c7a14d3df0449b1b36a45e9e4ea2f53db3630d
AmitRedSoftware <amit.ahuja@redsoftware.in>
2025-04-15 09:48:59 +0530

    update app store changes

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,36 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+// const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+
+// const defaultConfig = getDefaultConfig(__dirname);
+// const nodeLibs = require("node-libs-react-native");
+// const customConfig = {
+//   resolver: {
+//     extraNodeModules: {
+//       ...require("node-libs-react-native"), 
+//       stream: require.resolve("stream-browserify"), 
+//       buffer: require.resolve("react-native-buffer"),
+//       crypto: require.resolve("react-native-crypto"), 
+//       extraNodeModules: nodeLibs,
+     
+      
+//     },
+//   },
+// };
+
+// module.exports = mergeConfig(defaultConfig, customConfig);
+
+
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 
 const defaultConfig = getDefaultConfig(__dirname);
 const nodeLibs = require("node-libs-react-native");
+
 const customConfig = {
   resolver: {
     extraNodeModules: {
-      ...require("node-libs-react-native"), 
-      stream: require.resolve("stream-browserify"), 
-      buffer: require.resolve("react-native-buffer"),
+      ...nodeLibs, // Load all necessary node modules
+      stream: require.resolve("readable-stream"), // Required for some crypto functions
+      buffer: require.resolve("buffer"), // Buffer polyfill
       crypto: require.resolve("crypto-browserify"),
-      extraNodeModules: nodeLibs,
-     
-      
     },
   },
 };
